8c659b79af1f7654febac47e4625d2a5895e1d6c
Emmett Underhill <emmettu@gmail.com>
2016-08-10 23:26:58 -0400

    Adding more packages.

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+(defun go-go-emacs ()
+  (setup-package-management)
+  (bootstrap-use-package)
+  (prettify)
+  (install-plugins))
+
 (defun setup-package-management ()
   (require 'package)
   (setq package-enable-at-startup nil)
@@ -22,9 +28,30 @@
   ;; Remove GUI things
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (scroll-bar-mode -1))
+  (scroll-bar-mode -1)
+  (setup-theme)
+  (setup-smart-modeline))
 
-(defun setup-evil ())
+(defun setup-smart-modeline ()
+  (use-package smart-mode-line
+               :init
+               (setq sml/no-confirm-load-theme t)
+               (sml/setup)))
+
+(defun setup-theme ()
+  (use-package solarized-theme
+               :init (load-theme 'solarized-dark t)))
+
+(defun setup-evil ()
+  (use-package key-chord)
+  (use-package evil-escape)
+  (use-package evil
+               :init
+               (evil-mode 1)
+               (key-chord-mode 1)
+               (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+               (evil-escape-mode)
+               (setq-default evil-escape-key-sequence "jk")))
 
 (defun setup-helm ()
   (use-package helm
@@ -42,16 +69,27 @@
 
 (defun setup-yasnippets ()
   (use-package yasnippet
+               :defer t
                :init
                (yas-global-mode 1)))
+
+(defun setup-company ()
+  (use-package company
+               :defer t
+               :config (global-company-mode)))
+
+(defun setup-haskell ()
+  (use-package haskell-mode
+               :mode ("\\.hs\\'" "\\.lhs\\'")
+               :config
+               (add-hook 'haskell-mode-hook 'interactive-haskell-mode)))
 
 (defun install-plugins ()
   (setup-evil)
   (setup-yasnippets)
   (setup-helm)
-  (setup-projectile))
+  (setup-projectile)
+  (setup-company)
+  (setup-haskell))
 
-(prettify)
-(setup-package-management)
-(bootstrap-use-package)
-(install-plugins)
+(go-go-emacs)
