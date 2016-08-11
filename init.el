42d24d8ba975aff0e28cfc364fe959ee3450291b
Emmett Underhill <emmettu@gmail.com>
2016-08-10 23:52:59 -0400

    Fix indenting

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
   (setq package-enable-at-startup nil)
 
   (add-to-list 'package-archives
-               '("melpa" . "https://melpa.org/packages/") t)
+	       '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize))
 
 (defun bootstrap-use-package ()
@@ -34,55 +34,59 @@
 
 (defun setup-smart-modeline ()
   (use-package smart-mode-line
-               :init
-               (setq sml/no-confirm-load-theme t)
-               (sml/setup)))
+    :init
+    (setq sml/no-confirm-load-theme t)
+    (sml/setup)))
 
 (defun setup-theme ()
   (use-package solarized-theme
-               :init (load-theme 'solarized-dark t)))
+    :init (load-theme 'solarized-dark t)))
 
 (defun setup-evil ()
   (use-package key-chord)
   (use-package evil-escape)
+  (setup-evil-leader)
   (use-package evil
-               :init
-               (evil-mode 1)
-               (key-chord-mode 1)
-               (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-               (evil-escape-mode)
-               (setq-default evil-escape-key-sequence "jk")))
+    :init
+    (evil-mode 1)
+    (key-chord-mode 1)
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+    (evil-escape-mode)
+    (setq-default evil-escape-key-sequence "jk")))
+
+(defun setup-evil-leader ()
+  (use-package evil-leader))
 
 (defun setup-helm ()
   (use-package helm
-               :init
-               (helm-mode 1)
-               (define-key helm-map (kbd "C-k") #'helm-previous-line)
-               (define-key helm-map (kbd "C-j") #'helm-next-line)
-               (define-key helm-find-files-map (kbd "C-h") #'helm-find-files-up-one-level)
-               (define-key helm-find-files-map (kbd "C-l") 'nil)
-               (define-key helm-map (kbd "C-l") #'helm-execute-persistent-action)))
+    :init
+    (helm-mode 1)
+    (define-key helm-map (kbd "C-k") #'helm-previous-line)
+    (define-key helm-map (kbd "C-j") #'helm-next-line)
+    (define-key helm-find-files-map (kbd "C-h") #'helm-find-files-up-one-level)
+    (define-key helm-find-files-map (kbd "C-l") 'nil)
+    (define-key helm-map (kbd "C-l") #'helm-execute-persistent-action)))
 
 (defun setup-projectile ()
   (use-package helm-projectile
-               :commands (helm-projectile)))
+    :commands (helm-projectile)))
 
 (defun setup-yasnippets ()
   (use-package yasnippet
-               :defer t
-               :init
-               (yas-global-mode 1)))
+    :defer t
+    :init
+    (yas-global-mode 1)))
 
 (defun setup-company ()
   (use-package company
-               :defer t
-               :config (global-company-mode)))
+    :defer t
+    :config (global-company-mode)))
 
 (defun setup-haskell ()
   (use-package haskell-mode
-               :mode ("\\.hs\\'" "\\.lhs\\'")
-               :config
-               (add-hook 'haskell-mode-hook 'interactive-haskell-mode)))
+    :mode ("\\.hs\\'" "\\.lhs\\'")
+    :config
+    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)))
 
 (defun install-plugins ()
   (setup-evil)
