b32b13559d5335a4171b21e1b4d2fee8ab61761f
Emmett Underhill <emmettu@gmail.com>
2016-08-13 01:28:53 -0400

    Adding mu4e

--- init.el
+++ init.el
@@ -221,6 +221,65 @@
   (circe "Freenode")
   (circe "vanaltj"))
 
+(defun setup-mu4e ()
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+  (use-package mu4e
+  :ensure nil
+    :commands (mu4e)
+    :config
+    (mu4e-init)))
+
+(defun mu4e-init ()
+  ;; default
+  (setq mu4e-maildir "~/.mail/gmail")
+
+  (setq mu4e-drafts-folder "/[Gmail]/.Drafts")
+  (setq mu4e-sent-folder   "/[Gmail]/.Sent Mail")
+  (setq mu4e-trash-folder  "/[Gmail]/.Trash")
+
+  ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+  (setq mu4e-sent-messages-behavior 'delete)
+
+  ;; (See the documentation for `mu4e-sent-messages-behavior' if you have
+  ;; additional non-Gmail addresses and want assign them different
+  ;; behavior.)
+
+  ;; setup some handy shortcuts
+  ;; you can quickly switch to your Inbox -- press ``ji''
+  ;; then, when you want archive some messages, move them to
+  ;; the 'All Mail' folder by pressing ``ma''.
+
+  (setq mu4e-maildir-shortcuts
+	'( ("/INBOX"               . ?i)
+	   ("/[Gmail]/.Sent Mail"   . ?s)
+	   ("/[Gmail]/.Trash"       . ?t)
+	   ("/[Gmail]/.All Mail"    . ?a)))
+
+  ;; allow for updating mail using 'U' in the main view:
+  (setq mu4e-get-mail-command "mbsync gmail")
+
+  ;; something about ourselves
+  (setq
+   user-mail-address "eunderhi@ualberta.ca"
+   user-full-name  "Emmett Underhill"
+   mu4e-compose-signature "Emmett Underhill")
+
+   ;; sending mail -- replace USERNAME with your gmail username
+   ;; also, make sure the gnutls command line utils are installed
+   ;; package 'gnutls-bin' in Debian/Ubuntu
+
+   (use-package smtpmail
+     :init
+     ;; alternatively, for emacs-24 you can use:
+     (setq message-send-mail-function 'smtpmail-send-it
+	   smtpmail-stream-type 'starttls
+	   smtpmail-default-smtp-server "smtp.gmail.com"
+	   smtpmail-smtp-server "smtp.gmail.com"
+	   smtpmail-smtp-service 587))
+
+   ;; don't keep message buffers around
+   (setq message-kill-buffer-on-exit t))
+
 (defun install-plugins ()
   (setup-evil)
   (setup-yasnippets)
@@ -231,7 +290,8 @@
   (setup-magit)
   (setup-shells)
   (setup-persp)
-  (setup-circe))
+  (setup-circe)
+  (setup-mu4e))
 
 (defun set-global-bindings ()
   (global-set-key (kbd "M-h") 'evil-window-left)
